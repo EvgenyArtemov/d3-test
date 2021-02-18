1678d5ddc4a433d571726ad9a4b11cff14f1ebdd
Evgeniy Artemov <e.artemov@visionlabs.ru>
2021-02-18 22:07:13 +0300

    second example

--- src/main.js
+++ src/main.js
@@ -1,10 +1,36 @@
 import '../src/SCSS/base.scss';
 import * as d3 from "d3";
+import movies from './movies.json';
 
 const data = [45, 67, 96, 84, 41];
+let movieData = [];
+movieData = Object.entries(movies);
 const rectWidth = 50;
 
-function drawPetal() {
+const colors = {
+  Action: "#ffc8f0",
+  Comedy: "#cbf2bd",
+  Animation: "#afe9ff",
+  Drama: "#ffb09e",
+  Other: "#FFF2B4",
+}
+
+const petals = {
+  G: "M0 0 C50 50 50 100 0 100 C-50 100 -50 50 0 0",
+  PG: "M-35 0 C-25 25 25 25 35 0 C50 25 25 75 0 100 C-25 75 -50 25 -35 0",
+  "PG-13": "M0,0 C50,40 50,70 20,100 L0,85 L-20,100 C-50,70 -50,40 0,0",
+  R: "M0 0 C50 25 50 75 0 100 C-50 75 -50 25 0 0",
+}
+
+const pathWidth = 120
+const pathHeight = 120
+const perRow = 7
+
+const calculateGridPos = (i) => {
+  return [(i % perRow + 0.5) * pathWidth, (Math.floor(i / perRow) + 0.5) * pathWidth]
+}
+
+function drawChart() {
   // const smileyPath = 'M0,0 C50,40 50,70 20,100 L0,85 L-20,100 C-50,70 -50,40 0,0'
   const htmlSvg = `
   <svg id='container' width=${rectWidth * data.length} height=100 style='border: 1px dashed' >
@@ -30,4 +56,30 @@ function drawPetal() {
     .attr('fill', 'pink')
 };
 
-drawPetal();
+function drawPetals() {
+  const htmlSvg = `<svg width=500 height=100 style='border: 1px dashed'>
+      <path transform='translate(50, 0)' />
+      <path transform='translate(150, 0)' />
+      <path transform='translate(250, 0)' />
+      <path transform='translate(350, 0)' />
+      <path transform='translate(450, 0)' />
+    </svg>`;
+
+  const container = document.querySelector('div.petals_container2');
+  container.innerHTML = htmlSvg;
+
+  const svg = d3.select(container);
+
+  const selectAll = svg.selectAll('path')
+    .data(movieData)
+    .attr('d', (d) => petals[d[1].Rated])
+    .attr('fill', (d) => colors[d[1].Genre.split(',')[0]] || colors.Other)
+    .attr('fill-opacity', 0.5)
+    .attr('stroke', (d) => colors[d[1].Genre.split(',')[0]] || colors.Other)
+
+  console.log(selectAll)
+}
+
+drawChart();
+
+drawPetals()
