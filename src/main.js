6f8956c23c738f046e3aeca6dcd1e8ef95f64bd9
Evgeniy Artemov <e.artemov@visionlabs.ru>
2021-04-13 00:04:50 +0300

    second lesson final

--- src/main.js
+++ src/main.js
@@ -34,7 +34,7 @@ const calculateGridPos = (i) => {
 function drawChart() {
   // const smileyPath = 'M0,0 C50,40 50,70 20,100 L0,85 L-20,100 C-50,70 -50,40 0,0'
   const htmlSvg = `
-  <svg id='container' width=${rectWidth * data.length} height=100 style='border: 1px dashed' >
+  <svg id='svgCont' width=${rectWidth * data.length} height=100 style='border: 1px dashed' >
     <rect />
     <rect />
     <rect />
@@ -42,7 +42,7 @@ function drawChart() {
     <rect />
   </svg>
   `;
-  const container = document.querySelector('div.petals_container');
+  const container = document.querySelector('div.chart_container');
   container.innerHTML = htmlSvg;
 
   const svg = d3.select(container);
@@ -61,7 +61,7 @@ function drawPetals() {
 
   const htmlSvg = `<svg id="svgCont" width=${perRow * pathWidth} height=${svgHeight} style='border: 1px dashed'></svg>`;
 
-  const container = document.querySelector('div.petals_container2');
+  const container = document.querySelector('div.petals_container');
   container.innerHTML = htmlSvg;
 
   const svg = d3.select(container).select('#svgCont');
@@ -76,6 +76,39 @@ function drawPetals() {
 
 }
 
-drawChart();
+const data2 = [98, 34, 23, 58, 75, 10, 45, 91];
 
+function drawScaledChart() {
+  const htmlSvg = `
+  <svg id='container' width=${rectWidth * data2.length} height=200 style='border: 1px dashed' >
+  </svg>
+  `;
+  const container = document.querySelector('div.scaledChart_container');
+  container.innerHTML = htmlSvg;
+
+  const xScale = d3.scaleBand()
+    .domain(["0", "1", "2", "3", "4", "5", "6", "7", "8"])
+    .range([0, rectWidth * data2.length])
+    .padding(0.35)
+
+  const maxDataValue = d3.max(data2, d => d);
+
+  const yScale = d3.scaleLinear()
+    .domain([0, maxDataValue])
+    .range([200, 0]) // 200 it's predefined height
+
+  const svg = d3.select(container).select('svg');
+  const selectAll = svg.selectAll('rect')
+    .data(data2).enter().append('rect')
+    .attr('x', (d, i) => xScale(i))
+    .attr('y', (d, i) => yScale(d))
+    .attr('height', (d) => d)
+    .attr('width', rectWidth)
+    .attr('stroke-width', 3)
+    .attr('stroke', 'plum')
+    .attr('fill', 'pink')
+};
+
+drawChart();
+drawScaledChart();
 drawPetals()
